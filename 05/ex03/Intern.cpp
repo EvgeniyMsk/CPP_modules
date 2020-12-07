f481f5b50111589d56b98d07972056f3ff092d76
Qsymond <logvinov.evgeniy@yandex.ru>
2020-12-07 19:19:07 +0300

    all are fixed )

--- 05/ex03/Intern.cpp
+++ 05/ex03/Intern.cpp
@@ -30,7 +30,7 @@ Form *Intern::makeForm(std::string type, std::string target) {
 	for (int i = 0; i < 3; i++)
 		if (type == types[i])
 		{
-			std::cout << "Интеррн создал " << type << std::endl;
+			std::cout << "Интерн создал " << type << std::endl;
 			return(this->*func[i])(target);
 		}
 	std::cout << "Интерн не может создать " << type << " так как не умеет!" << std::endl;
